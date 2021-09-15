140079451fdb8a78602761e1358f5b5400f682e1
Mr. Clarke <thomas.clarke@dvusd.org>
2021-09-15 12:33:59 -0700

    Initial Version

---
+++ Game/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Ghost1 = New System.Windows.Forms.PictureBox()
+        Me.Wall2 = New System.Windows.Forms.PictureBox()
+        Me.Wall4 = New System.Windows.Forms.PictureBox()
+        Me.Wall3 = New System.Windows.Forms.PictureBox()
+        Me.Wall1 = New System.Windows.Forms.PictureBox()
+        Me.Avatar = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Ghost2 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.Ghost1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Wall2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Wall4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Wall3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Wall1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Avatar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Ghost2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Ghost1
+        '
+        Me.Ghost1.Image = Global.WindowsApp1.My.Resources.Resources.ghost
+        Me.Ghost1.Location = New System.Drawing.Point(40, 288)
+        Me.Ghost1.Name = "Ghost1"
+        Me.Ghost1.Size = New System.Drawing.Size(38, 37)
+        Me.Ghost1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Ghost1.TabIndex = 5
+        Me.Ghost1.TabStop = False
+        '
+        'Wall2
+        '
+        Me.Wall2.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.Wall2.Location = New System.Drawing.Point(175, 264)
+        Me.Wall2.Name = "Wall2"
+        Me.Wall2.Size = New System.Drawing.Size(92, 14)
+        Me.Wall2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Wall2.TabIndex = 4
+        Me.Wall2.TabStop = False
+        '
+        'Wall4
+        '
+        Me.Wall4.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.Wall4.Location = New System.Drawing.Point(328, 137)
+        Me.Wall4.Name = "Wall4"
+        Me.Wall4.Size = New System.Drawing.Size(10, 141)
+        Me.Wall4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Wall4.TabIndex = 3
+        Me.Wall4.TabStop = False
+        '
+        'Wall3
+        '
+        Me.Wall3.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.Wall3.Location = New System.Drawing.Point(246, 137)
+        Me.Wall3.Name = "Wall3"
+        Me.Wall3.Size = New System.Drawing.Size(92, 14)
+        Me.Wall3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Wall3.TabIndex = 2
+        Me.Wall3.TabStop = False
+        '
+        'Wall1
+        '
+        Me.Wall1.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.Wall1.Location = New System.Drawing.Point(175, 137)
+        Me.Wall1.Name = "Wall1"
+        Me.Wall1.Size = New System.Drawing.Size(10, 141)
+        Me.Wall1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Wall1.TabIndex = 1
+        Me.Wall1.TabStop = False
+        '
+        'Avatar
+        '
+        Me.Avatar.Image = Global.WindowsApp1.My.Resources.Resources.Pac1
+        Me.Avatar.Location = New System.Drawing.Point(40, 241)
+        Me.Avatar.Name = "Avatar"
+        Me.Avatar.Size = New System.Drawing.Size(38, 37)
+        Me.Avatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Avatar.TabIndex = 0
+        Me.Avatar.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 33)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(471, 10)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.PictureBox2.Location = New System.Drawing.Point(24, 331)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(471, 10)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.PictureBox3.Location = New System.Drawing.Point(485, 33)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(10, 306)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.WindowsApp1.My.Resources.Resources.wall
+        Me.PictureBox4.Location = New System.Drawing.Point(24, 35)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(10, 306)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 9
+        Me.PictureBox4.TabStop = False
+        '
+        'Ghost2
+        '
+        Me.Ghost2.Image = Global.WindowsApp1.My.Resources.Resources.ghost
+        Me.Ghost2.Location = New System.Drawing.Point(40, 288)
+        Me.Ghost2.Name = "Ghost2"
+        Me.Ghost2.Size = New System.Drawing.Size(38, 37)
+        Me.Ghost2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Ghost2.TabIndex = 10
+        Me.Ghost2.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Ghost2)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Ghost1)
+        Me.Controls.Add(Me.Wall2)
+        Me.Controls.Add(Me.Wall4)
+        Me.Controls.Add(Me.Wall3)
+        Me.Controls.Add(Me.Wall1)
+        Me.Controls.Add(Me.Avatar)
+        Me.Name = "Form1"
+        Me.Text = "Sample"
+        CType(Me.Ghost1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Wall2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Wall4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Wall3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Wall1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Avatar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Ghost2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Avatar As PictureBox
+    Friend WithEvents Wall1 As PictureBox
+    Friend WithEvents Wall3 As PictureBox
+    Friend WithEvents Wall4 As PictureBox
+    Friend WithEvents Wall2 As PictureBox
+    Friend WithEvents Ghost1 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Ghost2 As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Game/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+    Dim facingLeft As Boolean
+    Dim facingUp As Boolean
+    Dim points(99) As Point
+    Dim pacIndex As Integer
+    Dim ghostIndex As Integer
+    Dim ghostDirection As Integer
+    Dim pacdirection As String = "E"
+
+    Dim r As New Random
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Left
+                movehorizontal(-10, Avatar)
+                ChangeDirection(Avatar, "W")
+            Case Keys.Right
+                ChangeDirection(Avatar, "E")
+                movehorizontal(10, Avatar)
+            Case Keys.Up
+                ChangeDirection(Avatar, "N")
+                movevertical(-10, Avatar)
+            Case Keys.Down
+                ChangeDirection(Avatar, "S")
+                movevertical(10, Avatar)
+        End Select
+        points(pacIndex) = Avatar.Location
+        pacIndex = pacIndex + 1
+        pacIndex = pacIndex Mod 100
+
+        'start ghost after 20 moves
+        If pacIndex > 20 Then
+            Timer1.Enabled = True
+        End If
+        Me.Refresh()
+    End Sub
+    Sub ChangeDirection(p As PictureBox, dir As String)
+        'No change in direction
+        If pacdirection = dir Then Return
+
+        'Get back to facing right
+        Select Case pacdirection
+            Case "W"
+                p.Image.RotateFlip(RotateFlipType.RotateNoneFlipX)
+            Case "N"
+                p.Image.RotateFlip(RotateFlipType.Rotate90FlipNone)
+            Case "S"
+                p.Image.RotateFlip(RotateFlipType.Rotate270FlipNone)
+        End Select
+
+        Select Case dir
+            Case "W"
+                p.Image.RotateFlip(RotateFlipType.RotateNoneFlipX)
+            Case "N"
+                p.Image.RotateFlip(RotateFlipType.Rotate270FlipNone)
+            Case "S"
+                p.Image.RotateFlip(RotateFlipType.Rotate90FlipNone)
+        End Select
+        pacdirection = dir
+
+    End Sub
+
+    Function movehorizontal(dist As Integer, p As PictureBox) As Boolean
+        Dim previous As Point = p.Location
+
+        p.Location = New Point(p.Location.X + dist, p.Location.Y)
+        'If we ran into a wall put the previous location back
+        If CollidesWithWall(p) Then
+            p.Location = previous
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Function movevertical(dist As Integer, p As PictureBox) As Boolean
+        Dim previous As Point = p.Location
+        p.Location = New Point(p.Location.X, p.Location.Y + dist)
+        'If we ran into a wall put the previous location back
+        If CollidesWithWall(p) Then
+            p.Location = previous
+            Return False
+        End If
+        Return True
+    End Function
+
+    Function CollidesWithWall(p As PictureBox) As Boolean
+        For Each PictureBox In Me.Controls
+            If PictureBox IsNot p AndAlso p.Bounds.IntersectsWith(PictureBox.Bounds) Then
+                CollidesWithWall = True
+                If p Is Avatar And (
+                   PictureBox Is Ghost1 Or
+                   PictureBox Is Ghost2
+                   ) Then
+                    MessageBox.Show("You loose!")
+                    ResetGame()
+                End If
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
+    Sub ResetGame()
+        Avatar.Location = New Point(40, 240)
+        pacIndex = 0
+        Timer1.Enabled = False
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Ghost1.Location = points(ghostIndex)
+        ghostIndex += 1
+        ghostIndex = ghostIndex Mod 100
+        If ghostDirection = 0 Then
+            If Not movehorizontal(10, Ghost2) Then
+                ghostDirection = r.Next(0, 4)
+            End If
+        End If
+        If ghostDirection = 1 Then
+            If Not movehorizontal(-10, Ghost2) Then
+                ghostDirection = r.Next(0, 4)
+            End If
+        End If
+        If ghostDirection = 2 Then
+            If Not movevertical(10, Ghost2) Then
+                ghostDirection = r.Next(0, 4)
+            End If
+        End If
+        If ghostDirection = 3 Then
+            If Not movevertical(-10, Ghost2) Then
+                ghostDirection = r.Next(0, 4)
+            End If
+        End If
+    End Sub
+End Class
