9556e4acc6b1b2e41a4cb2990415aa46bcdec303
Mr. Clarke <thomas.clarke@dvusd.org>
2022-03-09 12:24:08 -0700

    Fixed the width

--- DrawingApp/Form1.vb
+++ DrawingApp/Form1.vb
@@ -54,4 +54,8 @@
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         c = sender.backcolor
     End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        w = TrackBar1.Value
+    End Sub
 End Class
