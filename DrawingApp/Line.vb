d72e707fc5de255ecbf3164496cd71281979d2c2
Mr. Clarke <thomas.clarke@dvusd.org>
2022-03-24 22:12:22 -0700

    Added polygon example.

--- DrawingApp/Line.vb
+++ DrawingApp/Line.vb
@@ -12,7 +12,17 @@
     End Sub
     Public Sub Draw()
         Using g As Graphics = Graphics.FromImage(m_image)
-            g.DrawLine(Pen, m_a, m_b)
+            '  g.DrawLine(Pen, m_a, m_b)
+            'g.DrawArc(Pen, m_a.X, m_a.Y, 100, 100, 0, 90)
+            '  g.DrawPie(Pen, m_a.X, m_a.Y, 100, 100, 0, 90)
+            '  g.DrawRectangle(Pen, m_a.X, m_a.Y, 100, 100)
+            Dim points(2) As Point
+            points(0) = New Point(m_a.X, m_a.Y)
+            points(1) = New Point(m_a.X, m_a.Y + 50)
+            points(2) = New Point(m_a.X + 50, m_a.Y)
+
+            g.DrawPolygon(Pen, points)
+            'g.FillRectangle(New SolidBrush(Pen.Color), m_a.X, m_a.Y, 100, 100)
         End Using
 
     End Sub
